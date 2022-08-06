68c678e4f5af54358898473abd2060cf399b4e72
MrHoran8120 <garyhoran@users.noreply.github.com>
2022-08-06 20:53:27 +1000

    Add project files.

---
+++ WindowsFormsApp12/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(2, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Click me to create labels";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Font Size";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar1.Location = new System.Drawing.Point(172, 404);
+            this.trackBar1.Maximum = 40;
+            this.trackBar1.Minimum = 3;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 20;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.trackBar2.Location = new System.Drawing.Point(282, 404);
+            this.trackBar2.Maximum = 50;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(149, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 10;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 375);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of Buttons";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp12/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        List<Label> myLabels = new List<Label>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            createLabels(10);
+            calculatePositions();
+
+        }
+
+        // Create all of the labels and put them into the list
+        void createLabels(int numberOfLabels)
+        {
+            for (int i = 0; i < numberOfLabels; i++)
+            {
+                Label x = new Label();
+                this.Controls.Add(x);
+                myLabels.Add(x);
+                x.Text = "TEST" + i;
+                x.Font = new Font("Arial", trackBar1.Value, FontStyle.Bold);
+                x.BackColor = Color.Blue;
+                x.ForeColor = Color.White;
+                x.AutoSize = true;
+            }
+        }
+
+        // workout the positions of the labels
+        void calculatePositions()
+        {
+            Point theLoc = new Point(0, 0);
+            Size winSize = this.Size;
+            int col = 0;
+            int row = 1;
+            const int gap = 10;
+            foreach (Label label in myLabels)
+            {
+
+                // firstly see whether or not the next label will fit inside this row
+                // if not we increment row.
+                if ((((col + 1) * label.Size.Width) + (col * gap)) > winSize.Width)
+                {
+                    col = 0;
+                    row++;
+                }
+
+                theLoc.X = (int)((col * label.Size.Width) + (col * gap));
+                theLoc.Y = (int)((row * (label.Size.Height + 20)) + (row * gap));
+                label.Location = theLoc;
+
+                col++;
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            calculatePositions();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            changeFontSize(trackBar1.Value);
+            calculatePositions();
+        }
+        private void changeFontSize(int newSize)
+        {
+            Font newFont = new Font("Arial", newSize);
+            foreach (Label label in myLabels)
+            {
+                label.Font = newFont;
+            }
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            foreach (Label theLab in myLabels)
+            {
+                this.Controls.Remove(theLab);
+            }
+            myLabels.Clear();
+            createLabels(trackBar2.Value);
+            calculatePositions();
+
+        }
+    }
+}
